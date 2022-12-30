bef4c27bc96414c75e8c9e3d3bc096c749294808
Shape Lee <shape1026@gmail.com>
2022-12-30 15:33:03 +0900

    [221229] react setTimeout 이용

--- 04.respons-check/ResponseCheck.jsx
+++ 04.respons-check/ResponseCheck.jsx
@@ -9,8 +9,40 @@ class ResponseCheck extends Component {
     result: [],
   }
 
-  onChangeScreen = () => {
+  timeout;
+  startTime;
+  endTime;
 
+  onChangeScreen = () => {
+    const { state, message, result } = this.state;
+    if (state === 'waiting') {
+      this.setState({
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요.',
+      })
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: 'now',
+          message: '지금 클릭',
+        })
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000);
+    } else if (state === 'ready') {
+      clearTimeout(this.timeout);
+      this.setState({
+        state: 'waiting',
+        message: '너무 성급하셨어요. 초록색이 되면 클릭하세요.'
+      });
+    } else if (state === 'now') {
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          message: '클릭해서 시작하세요.',
+          result: [...prevState.result, this.endTime - this.startTime],
+        }
+      });
+    }
   }
 
   renderAverage = () => {
@@ -21,7 +53,7 @@ class ResponseCheck extends Component {
   }
 
   render() {
-    const { state, message } = this.state
+    const { state, message, result } = this.state
     return(
       <>
         <div id="screen" className={state} onClick={this.onChangeScreen}>
