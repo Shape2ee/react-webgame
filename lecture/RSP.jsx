990c522744706eac59af6ffa54740dbfed609a2b
Shape Lee <shape1026@gmail.com>
2023-01-03 22:45:38 +0900

    Feat: Add RSP onClickBtn, setTimeout

--- lecture/RSP.jsx
+++ lecture/RSP.jsx
@@ -7,15 +7,22 @@ import React, { Component } from 'react';
 
 const rspCoords = {
   rock: '0',
-  scissors: '-142px',
+  scissor: '-142px',
   paper: '-284px',
 }
 
-const score = {
+const scores = {
   rock: 0,
-  scissors: 1,
+  scissor: 1,
   paper: -1,
 }
+
+const computerChoice = (imgCoord) => {
+  console.log(imgCoord)
+  return Object.entries(rspCoords).find((v) => {
+    return v[1] === imgCoord;
+  })[0];
+};
 
 class RSP extends Component {
   state = {
@@ -25,36 +32,71 @@ class RSP extends Component {
   }
 
   interval;
+  timeout;
+  clickable = true;
 
   componentDidMount() { // 컴포넌트가 첫 렌더링 후, 여기에 비동기 요청을 많이 함
-    this.interval = setInterval(() => {
-      const { imgCoord } = this.state;
-      if (imgCoord === rspCoords.rock) {
-        this.setState({
-          imgCoord: rspCoords.scissors
-        })  
-      } else if (imgCoord === rspCoords.scissors) {
-        this.setState({
-          imgCoord: rspCoords.paper
-        })
-      } else if (imgCoord === rspCoords.paper) {
-        this.setState({
-          imgCoord: rspCoords.rock
-        })
-      }
-    }, 1000)
+    console.log('here')
+    this.interval = setInterval(this.changeHand, 100)
   }
 
-  componentDidUpdate() { // 리렌더링 후
-
-  }
+  // componentDidUpdate() { 리렌더링 후
+  // }
 
   componentWillUnmount() { // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 함
-
+    clearInterval(this.interval)
   }
 
-  onClick = () => {
+  changeHand = () => {
+    // console.log('here')
+    const { imgCoord } = this.state;
+    if (imgCoord === rspCoords.rock) {
+      this.setState({
+        imgCoord: rspCoords.scissor,
+      })  
+    } else if (imgCoord === rspCoords.scissor) {
+      this.setState({
+        imgCoord: rspCoords.paper,
+      })
+    } else if (imgCoord === rspCoords.paper) {
+      this.setState({
+        imgCoord: rspCoords.rock,
+      })
+    }
+  }
 
+  onClickBtn = (choice) => {
+    const { imgCoord } = this.state;
+    if(this.clickable) {
+      clearInterval(this.interval);
+      this.clickable = false;
+      const myScore = scores[choice];
+      const computerScore = scores[computerChoice(imgCoord)];
+      const diff = myScore - computerScore;
+      if (diff === 0) {
+        this.setState({
+          result: '비겼습니다',
+        })
+      } else if ([-1, 2].includes(diff)) {
+        this.setState((prevState) => {
+          return {
+            result: '이겼습니다',
+            score: prevState.score + 1,
+          }
+        })
+      } else {
+        this.setState((prevState) => {
+          return {
+            result: '졌습니다',
+            score: prevState.score - 1,
+          }
+        })
+      }
+      this.timeout = setTimeout(() => {
+        this.clickable = true;
+        this.interval = setInterval(this.changeHand, 100)
+      }, 2000)
+    }
   }
 
   render() {
@@ -63,9 +105,9 @@ class RSP extends Component {
       <>
         <div id='computer' style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}}></div>
         <div>
-          <button id='rock' className='btn' onClick={() => this.onClickBtn('바위')} >바위</button>
-          <button id='scissor' className='btn' onClick={() => this.onClickBtn('가위')} >가위</button>
-          <button id='paper' className='btn' onClick={() => this.onClickBtn('보')} >보</button>
+          <button id='rock' className='btn' onClick={() => this.onClickBtn('rock')} >바위</button>
+          <button id='scissor' className='btn' onClick={() => this.onClickBtn('scissor')} >가위</button>
+          <button id='paper' className='btn' onClick={() => this.onClickBtn('paper')} >보</button>
         </div>
         <div>{result}</div>
         <div>현재 {score}점</div>
