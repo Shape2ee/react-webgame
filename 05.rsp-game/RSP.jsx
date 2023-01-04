3f8f9f18a5ea6dc06421ebfee11d124f97872a67
Shape Lee <shape1026@gmail.com>
2023-01-04 23:16:56 +0900

    Feat: class에서 hooks로 변경

---
+++ 05.rsp-game/RSP.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+const rspCoords = {
+  rock: '0',
+  scissor: '-142px',
+  paper: '-284px',
+}
+
+const scores = {
+  rock: 0,
+  scissor: 1,
+  paper: -1,
+}
+
+const computerChoice = (imgCoord) => {
+  console.log(imgCoord)
+  return Object.entries(rspCoords).find((v) => {
+    return v[1] === imgCoord;
+  })[0];
+};
+
+const RSP = () => {
+  const [ result, setResult ] = useState('');
+  const [ imgCoord, setImgCoord ] = useState('0');
+  const [ score, setScore ] = useState(0);
+  const interval = useRef(null);
+  const timeout = useRef(null);
+  let clickable = true;
+
+  useEffect(() => {
+    interval.current = setInterval(changeHand, 100)
+    return () => {
+      clearInterval(interval.current);
+    }
+  }, [imgCoord])
+
+  const changeHand = () => {
+    if (imgCoord === rspCoords.rock) {
+      setImgCoord(rspCoords.scissor);
+    } else if (imgCoord === rspCoords.scissor) {
+      setImgCoord(rspCoords.paper);
+    } else if (imgCoord === rspCoords.paper) {
+      setImgCoord(rspCoords.rock);
+    }
+  }
+
+  const onClickBtn = (choice) => () => {
+    if(clickable) {
+      clearInterval(interval.current);
+      clickable = false;
+      const myScore = scores[choice];
+      const computerScore = scores[computerChoice(imgCoord)];
+      const diff = myScore - computerScore;
+      if (diff === 0) {
+        setResult('비겼습니다.');
+      } else if ([-1, 2].includes(diff)) {
+        setResult('이겼습니다.');
+        setScore((prevScore) => prevScore + 1);
+      } else {
+        setResult('졌습니다.');
+        setScore((prevScore) => prevScore - 1);
+      }
+      timeout.current = setTimeout(() => {
+        clickable = true;
+        interval.current = setInterval(changeHand, 100)
+      }, 2000)
+    }
+  }
+
+  return (
+    <>
+      <div id='computer'
+        style={{background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}}>
+      </div>
+      <div>
+        {/* <button id='rock' className='btn' onClick={() => this.onClickBtn('rock')} >바위</button>
+        <button id='scissor' className='btn' onClick={() => this.onClickBtn('scissor')} >가위</button>
+        <button id='paper' className='btn' onClick={() => this.onClickBtn('paper')} >보</button> */}
+        <button id='rock' className='btn' onClick={onClickBtn('rock')} >바위</button>
+        <button id='scissor' className='btn' onClick={onClickBtn('scissor')} >가위</button>
+        <button id='paper' className='btn' onClick={onClickBtn('paper')} >보</button>
+      </div>
+      <div>{result}</div>
+      <div>현재 {score}점</div>
+    </>
+  )
+}
+
+export default RSP;
