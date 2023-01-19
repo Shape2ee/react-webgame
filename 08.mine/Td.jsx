242e3ab2c8cce0d35380adaa9b6aef13c2023f00
Shape Lee <shape1026@gmail.com>
2023-01-19 10:21:31 +0900

    Feat: Add getTdText & getTdStyle

--- 08.mine/Td.jsx
+++ 08.mine/Td.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TableContext, CODE } from './Mine';
 
-const Td = () => {
+const Td = ({ rowIndex, cellIndex }) => {
+  const getTdStyle = (code) => {
+    switch(code) {
+      case CODE.NORMAL: 
+      case CODE.MINE: 
+        return { 
+          background: '#444' 
+        }
+      default:
+        return {
+          background: '#fff'
+        }
+    }
+  }
+
+  const getTdText = (code) => {
+    switch (code) {
+      case CODE.NORMAL:
+        return ''
+      case CODE.MINE: 
+        return '★'
+      default: 
+        return ''
+    }
+  }
+
+  const { tableData } = useContext(TableContext)
   return (
-    <td>
-      
+    <td style={getTdStyle(tableData[rowIndex][cellIndex])}>
+      {getTdText(tableData[rowIndex][cellIndex])}
     </td>  
   )
 };
--- 08.mine/Tr.jsx
+++ 08.mine/Tr.jsx
@@ -2,12 +2,12 @@ import React, { useContext } from 'react';
 import { TableContext } from './Mine';
 import Td from './Td';
 
-const Tr = () => {
+const Tr = ({ rowIndex }) => {
   const { tableData } = useContext(TableContext);
 
   return (
     <tr>
-      {tableData[0] && Array(tableData[0].length).fill().map((td, i) => <Td key={`td${i}`} />)}
+      {tableData[0] && Array(tableData[0].length).fill().map((td, i) => <Td key={`td${i}`} rowIndex={rowIndex} cellIndex={i} />)}
     </tr>
   )
 };
